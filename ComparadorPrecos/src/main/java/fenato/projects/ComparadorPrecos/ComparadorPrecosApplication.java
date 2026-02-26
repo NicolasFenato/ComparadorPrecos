cfbb9aa7b2eb13602a5b3d5e4ace66e82f7dd2ac
Nicolas <nicfenato@gmail.com>
2026-02-26 15:57:44 -0300

    Adição do projeto Spring-Boot.

---
+++ ComparadorPrecos/src/main/java/fenato/projects/ComparadorPrecos/ComparadorPrecosApplication.java
@@ -0,0 +1,13 @@
+package fenato.projects.ComparadorPrecos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComparadorPrecosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComparadorPrecosApplication.class, args);
+	}
+
+}
